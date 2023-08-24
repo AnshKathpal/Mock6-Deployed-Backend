0be5ce155b7b83635a20f859dac7668b3dea8f0f
AnshKathpal <storage.2.3107@gmail.com>
2023-08-24 20:25:36 +0530

    deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ const blacklist = require("../blacklist")
 require("dotenv").config();
 
 
-const auth=(req,res,next)=>{
+const middleware=(req,res,next)=>{
     const token=req.headers.authorization?.split(" ")[1]
 
     if(token){
